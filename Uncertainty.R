5de993d8453a61cb8dd0ceb6102f33f831209db8
Mathieu <93939398+mathver@users.noreply.github.com>
2022-02-02 10:54:11 +0100

    Ajout du read.table

--- Uncertainty.R
+++ Uncertainty.R
@@ -1,5 +1,12 @@
 library(archive)
+
+
+
+
+
 tf <- tempfile() ; td <- tempdir()
-file.path <- "https://ec.europa.eu/eurostat/estat-navtree-portlet-prod/BulkDownloadListing?sort=1&downfile=comext%2FCOMEXT_DATA%2FPRODUCTS%2Ffull200101.7z"
+file.path <- "https://ec.europa.eu/eurostat/estat-navtree-portlet-prod/BulkDownloadListing?sort=1&downfile=comext%2FCOMEXT_DATA%2FPRODUCTS%2Ffull200301.7z"
 download.file( file.path , tf , mode = "wb" )
-archive_extract(tf)
+data <- archive_extract(tf)
+data2 <- read.table(data, sep = ",", header = T)
+
