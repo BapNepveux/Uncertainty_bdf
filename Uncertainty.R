4b90ff79f0a9e15e47f175cd7bd213dced63d09d
Baptiste NEPVEUX <baptiste.nepveux@icloud.com>
2022-02-17 19:03:31 +0100

    Fonction de telechargement des données

--- Uncertainty.R
+++ Uncertainty.R
@@ -1,6 +1,6 @@
 library(archive)
-
-
+library(rvest)
+library(tidyverse)
 
 
 
@@ -9,4 +9,33 @@ file.path <- "https://ec.europa.eu/eurostat/estat-navtree-portlet-prod/BulkDownl
 download.file( file.path , tf , mode = "wb" )
 data <- archive_extract(tf)
 data2 <- read.table(data, sep = ",", header = T)
+
+import_un_mois <- function(nom_du_fichier){
+  tf <- tempfile() ; td <- tempdir()
+  website <- "https://ec.europa.eu/eurostat/estat-navtree-portlet-prod/BulkDownloadListing?sort=1&downfile=comext%2FCOMEXT_DATA%2FPRODUCTS%2F"
+  file.path <- paste(website,nom_du_fichier,sep = "")
+  download.file( file.path , tf , mode = "wb" )
+  pre_data <- archive_extract(tf)
+  data <- read.table(pre_data, sep = ",", header = T)
+  return(data)
+}
+data2 <- import_un_mois("fullxixu202108.7z")
+recupere_noms_mois <- function(){
+  code_html_page<-read_html(url("https://ec.europa.eu/eurostat/estat-navtree-portlet-prod/BulkDownloadListing?sort=1&dir=comext%2FCOMEXT_DATA%2FPRODUCTS"))
+  string_1 <-pipe_et_jambe_de_bois %>% html_nodes("a") %>%html_text()
+  string_2<- string_1[grepl("full",string_1)]
+  return(string_2)
+}
+recupere_noms_mois()
+verifie_validite_du_nom <- function(){
+  
+}
+
+importe_data <- function(month ="all"){
+  # file can be either "all" or "last
+}
+
+#answer <- c( content, wrapper) permet de append
+#Roxygen package pour la doc des fonctions
+#https://stackoverflow.com/questions/7187442/filter-a-vector-of-strings-based-on-string-matching
 
